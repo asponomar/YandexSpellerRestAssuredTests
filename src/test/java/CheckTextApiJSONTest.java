840a0f02994db9b4bd8691f0a78f3b957c2a1748
Aleksandr Ponomar <asponomar@gmail.com>
2021-02-05 17:49:51 +0300

    Hardcoded test data and asserts

--- src/test/java/CheckTextApiJSONTest.java
+++ src/test/java/CheckTextApiJSONTest.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 
 import static enums.Options.*;
 
-public class CheckTextApiJSONTests {
+public class CheckTextApiJSONTest {
 
     @Test
     public void simpleCallWithCorrectWord() {
